b87f6cef1a8c6b0212d9e4816a39a37091bfaca6
TuanAnh <anh.tht@kiotviet.com>
2025-09-12 12:01:48 +0700

    update build step

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode, command }) => ({
   base: process.env.VITE_BASE_PATH || (command === 'build' && process.env.NODE_ENV === 'production' ? '/draggy-notes/' : '/'),
   server: {
     host: "::",
-    port: 8080,
+    port: 7000,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
